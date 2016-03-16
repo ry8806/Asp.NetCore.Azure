98c7a86da3a4b6a0ad55f630ef8f4108bb3dc07f
Ryan Southgate <ryansouthgate@live.com>
2016-03-16 08:52:30 +0000

    initial commit

---
+++ ConApp1/src/ConApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json")
+                                        .Build();
+
+            var connectionString = config["Azure:ConnectionString"];
+
+            CloudStorageAccount acc = CloudStorageAccount.Parse(connectionString);
+
+            Console.ReadLine();
+        }
+    }
+}
